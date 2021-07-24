3667f3c33abed43f6e51994fb41bd3f0e79e882c
Amit Singh <amitsingh@Amits-MacBook-Pro.local>
2021-07-24 19:15:05 +0530

    functions passing

---
+++ lib/answer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function
+      selectHandler; // selectHandler is nothing it is only a variable name containing Function datatype.
+
+  Answer(this.selectHandler);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text('Answer 1'),
+        onPressed: selectHandler, //function need to be pass without brackets
+      ),
+    );
+  }
+}
