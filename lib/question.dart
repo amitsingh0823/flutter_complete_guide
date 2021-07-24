57d795e305c643249712ab526d14394358743909
Amit Singh <amitsingh@Amits-MacBook-Pro.local>
2021-07-24 14:59:28 +0530

    made new class Question

---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  //const Question({ Key? key }) : super(key: key);
+  String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(questionText);
+  }
+}
